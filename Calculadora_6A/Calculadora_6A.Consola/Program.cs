0d20c6ca596c43d025f85ca67191be73337cbd82
Irani Hernandez <31052226+irani080596@users.noreply.github.com>
2017-08-22 19:26:34 -0500

    Add files via upload

---
+++ Calculadora_6A/Calculadora_6A.Consola/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calculadora_6A.Libreria;
+
+namespace Calculadora_6A.Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Metodos misMetodos = new Metodos();
+            Console.WriteLine(misMetodos.Saludo());
+            Console.WriteLine(misMetodos.Sumar(10, 45));
+            Console.WriteLine(misMetodos.Restar(10, 84));
+            Console.WriteLine(misMetodos.Multiplicar(10, 10));
+            Console.WriteLine(misMetodos.Dividir(150, 2));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Calculadora_6A/Calculadora_6A.Libreria/Metodos.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora_6A.Libreria
+{
+    public class Metodos
+    {
+/// <summary>
+/// Funcion que envia saludo
+/// </summary>
+/// <returns>Cadena de caracteres</returns>
+        public string Saludo()
+        {
+            return "HOLA MUNDO DE CARAMELO";
+        }
+        public float Sumar(float numero1, float numero2)
+        
+        {
+            var resultado = numero1 + numero2;
+            return resultado;
+           
+
+        }
+        public float Restar(float numero3, float numero4)
+        {
+            var resultado2 = numero3 - numero4;
+            return resultado2;
+        }
+        public float Multiplicar(float numero5, float numero6)
+        {
+            var resultado3 = numero5 * numero6;
+            return resultado3;
+        }
+        public float Dividir(float numero7, float numero8)
+        {
+            var resultado4 = numero7 / numero8;
+            return resultado4;
+        }
+    }
+}
